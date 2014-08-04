f27365504dbe3e5edec1e54830ff6123283eee25
Trevor Elkins <elkins.trevor@gmail.com>
2014-08-04 00:45:49 -0400

    added more explicit read/write methods to try and avoid using the general read/write

--- Library/src/main/java/com/trevore/parcist/Parcist.java
+++ Library/src/main/java/com/trevore/parcist/Parcist.java
@@ -1,10 +1,16 @@
 package com.trevore.parcist;
 
+import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.text.TextUtils;
 import android.util.ArrayMap;
+import android.util.SparseArray;
+import android.util.SparseBooleanArray;
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Trevor on 7/10/2014.
@@ -24,43 +30,33 @@ public class Parcist {
     }
 
     /**
-     * Writes an object to a Parcel and then marshalls it.
-     * @param object The object to marshall
-     * @return The byte representation of the object after marshalling
+     * Marshalls the parcel
+     * @param parcel The parcel to marshall
+     * @return The marshalled parcel
      */
-    public static byte[] write(Object object) {
-        Parcel parcel = Parcel.obtain();
-        parcel.writeValue(object);
+    private static byte[] getBytes(Parcel parcel) {
         byte[] bytes = parcel.marshall();
         parcel.recycle(); // not sure if needed or a good idea
         return bytes;
     }
 
-    public static <T extends Parcelable> byte[] writeTypedList(List<T> val) {
-        Parcel parcel = Parcel.obtain();
-        parcel.writeTypedList(val);
-        byte[] bytes = parcel.marshall();
-        parcel.recycle(); // not sure if needed or a good idea
-        return bytes;
+    public static Bundle readBundle(byte[] bytes) throws ParcistInvalidatedException {
+        try {
+            Parcel parcel = unmarshall(bytes);
+            return parcel.readBundle();
+        }
+        catch(Exception e) {
+            throw new ParcistInvalidatedException("Could not create Parcelable, you should invalidate your data.", e);
+        }
     }
 
-    public static <T extends Parcelable> byte[] writeTypedArray(T[] val) {
+    public static byte[] writeBundle(Bundle val) {
         Parcel parcel = Parcel.obtain();
-        parcel.writeTypedArray(val, 0);
-        byte[] bytes = parcel.marshall();
-        parcel.recycle(); // not sure if needed or a good idea
-        return bytes;
+        parcel.writeBundle(val);
+        return getBytes(parcel);
     }
 
-    /**
-     * Creates the given Parcelable class from marshalled data.
-     * @param bytes The marshalled data
-     * @param creator Creator used to restore class
-     * @param <T> Class to create
-     * @return The created class
-     * @throws ParcistInvalidatedException
-     */
-    public static <T> T read(byte[] bytes, Parcelable.Creator<T> creator) throws ParcistInvalidatedException {
+    public static <T extends Parcelable> T readParcelable(byte[] bytes, Parcelable.Creator<T> creator) throws ParcistInvalidatedException {
         try {
             Parcel parcel = unmarshall(bytes);
             return creator.createFromParcel(parcel);
@@ -70,8 +66,62 @@ public class Parcist {
         }
     }
 
+    public static byte[] writeParcelable(Parcelable val) {
+        Parcel parcel = Parcel.obtain();
+        val.writeToParcel(parcel, 0);
+        return getBytes(parcel);
+    }
+
+    public static Parcelable[] readParcelableArray(byte[] bytes, Class<Parcelable> clazz) throws ParcistInvalidatedException {
+        try {
+            Parcel parcel = unmarshall(bytes);
+            return parcel.readParcelableArray(clazz.getClassLoader());
+        }
+        catch(Exception e) {
+            throw new ParcistInvalidatedException("Could not create Parcelable, you should invalidate your data.", e);
+        }
+    }
+
+    public static byte[] writeParcelableArray(Parcelable[] val) {
+        Parcel parcel = Parcel.obtain();
+        parcel.writeParcelableArray(val, 0);
+        return getBytes(parcel);
+    }
+
+    public static <T extends Parcelable> void readTypedList(byte[] bytes, List<T> list, Parcelable.Creator<T> creator) throws ParcistInvalidatedException {
+        try {
+            Parcel parcel = unmarshall(bytes);
+            parcel.readTypedList(list, creator);
+        }
+        catch(Exception e) {
+            throw new ParcistInvalidatedException("Could not create Parcelable, you should invalidate your data.", e);
+        }
+    }
+
+    public static <T extends Parcelable> byte[] writeTypedList(List<T> val) {
+        Parcel parcel = Parcel.obtain();
+        parcel.writeTypedList(val);
+        return getBytes(parcel);
+    }
+
+    public static <T extends Parcelable> void readTypedArray(byte[] bytes, T[] array, Parcelable.Creator<T> creator) throws ParcistInvalidatedException {
+        try {
+            Parcel parcel = unmarshall(bytes);
+            parcel.readTypedArray(array, creator);
+        }
+        catch(Exception e) {
+            throw new ParcistInvalidatedException("Could not create Parcelable, you should invalidate your data.", e);
+        }
+    }
+
+    public static <T extends Parcelable> byte[] writeTypedArray(T[] val) {
+        Parcel parcel = Parcel.obtain();
+        parcel.writeTypedArray(val, 0);
+        return getBytes(parcel);
+    }
+
     /**
-     * Creates the given class from marshalled data.  This is the less preferred way of doing it compared to using the
+     * Creates the given class from marshalled data.  Use the more explicit methods in favor of this.
      * Parcelable Creator.
      * @param bytes The marshalled data
      * @param clazz Class to restore to
@@ -91,38 +141,13 @@ public class Parcist {
     }
 
     /**
-     * Populates the given list with the Parcelable data.
-     * @param bytes The marshalled data
-     * @param list Empty list to populate
-     * @param creator Creator used to restore class
-     * @param <T> Class to create
-     * @throws ParcistInvalidatedException
+     * Writes an object to a Parcel and then marshalls it.  Use the more explicit methods in favor of this.
+     * @param val The object to marshall
+     * @return The byte representation of the object after marshalling
      */
-    public static <T> void readTypedList(byte[] bytes, List<T> list, Parcelable.Creator<T> creator) throws ParcistInvalidatedException {
-        try {
-            Parcel parcel = unmarshall(bytes);
-            parcel.readTypedList(list, creator);
-        }
-        catch(Exception e) {
-            throw new ParcistInvalidatedException("Could not create Parcelable, you should invalidate your data.", e);
-        }
-    }
-
-    /**
-     * Populates the given array with the Parcelable data.
-     * @param bytes The marshalled data
-     * @param array Empty array to populate
-     * @param creator Creator used to restore class
-     * @param <T> Class to create
-     * @throws ParcistInvalidatedException
-     */
-    public static <T> void readTypedArray(byte[] bytes, T[] array, Parcelable.Creator<T> creator) throws ParcistInvalidatedException {
-        try {
-            Parcel parcel = unmarshall(bytes);
-            parcel.readTypedArray(array, creator);
-        }
-        catch(Exception e) {
-            throw new ParcistInvalidatedException("Could not create Parcelable, you should invalidate your data.", e);
-        }
+    public static byte[] write(Object val) {
+        Parcel parcel = Parcel.obtain();
+        parcel.writeValue(val);
+        return getBytes(parcel);
     }
 }
